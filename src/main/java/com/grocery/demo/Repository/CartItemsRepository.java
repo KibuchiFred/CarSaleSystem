c98db5c7bf4e5743370890235ce2f92ede6e0afd
KibuchiFred <fredkibuchi64@gmail.com>
2020-02-05 23:26:17 +0300

    added some cart functionality

--- src/main/java/com/grocery/demo/Repository/CartItemsRepository.java
+++ src/main/java/com/grocery/demo/Repository/CartItemsRepository.java
@@ -2,10 +2,17 @@ package com.grocery.demo.Repository;
 
 import com.grocery.demo.Model.CartItems;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CartItemsRepository extends JpaRepository<CartItems, Long> {
     List<CartItems> findByUsername(String userName);
+
+    @Modifying
+    @Query("SELECT  c.subTotal FROM CartItems c where c.username = :username")
+    List<Double> allPrices(@Param("username") String username);
 
 }
--- src/main/java/com/grocery/demo/Service/CartService.java
+++ src/main/java/com/grocery/demo/Service/CartService.java
@@ -4,10 +4,14 @@ import com.grocery.demo.Model.CartItems;
 import com.grocery.demo.Model.Product;
 import com.grocery.demo.Repository.CartItemsRepository;
 import com.grocery.demo.Repository.ProductRepository;
+import io.netty.util.internal.MathUtil;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -34,6 +38,15 @@ public class CartService {
 
         List<CartItems> cartItems = new ArrayList<>();
         cartItemsRepository.findByUsername(userName).forEach(cartItems::add);
+
+        List<Double> getPrices = cartItemsRepository.allPrices(userName);
+
+       int sum = 0;
+        for (double items : getPrices){
+            sum += items;
+        }
+        System.out.println("The total cart Items Price is: "+sum);
+
 
         return cartItems;
     }
